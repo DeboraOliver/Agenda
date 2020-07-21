717167c8ba8ef04abe15e7e75b341cb18193c0d1
Debora Oliveira <debora.ufabc@gmail.com>
2020-07-21 18:50:47 -0300

    Criado tela de listagem de eventos

--- agenda/settings.py
+++ agenda/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        #mostrar o caminho para o template
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
